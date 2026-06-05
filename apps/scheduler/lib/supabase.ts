5eb070b05b49d836b06ecaff8ea30226eb314cd3
mountetnabusiness <mountetnabusiness@gmail.com>
2026-06-05 01:41:31 -0400

    feat: message

--- apps/scheduler/lib/supabase.ts
+++ apps/scheduler/lib/supabase.ts
@@ -4,7 +4,7 @@ export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    cookies: {
+    cookieOptions: {
       domain: '.summitclient.io',
     },
   }
