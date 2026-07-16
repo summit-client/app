b3bc57763439a1a022966878aad19f6f13b6ca76
Dario Rashid <drashid2005@gmail.com>
2026-07-15 20:20:03 -0400

    Remove generated Next.js files

--- apps/client/.next/dev/server/middleware.js
+++
@@ -1,5 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/0m~-_next_dist_0ebvp1j._.js")
-R.c("server/chunks/[root-of-the-server]__12bg0ko._.js")
-R.m("[project]/node_modules/.pnpm/next@16.2.1_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/client/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@16.2.1_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/client/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
--- apps/client/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FsayfpTdAnElTGN5Zh7zsYeXiSOcarduFs1ALN3tyWg=\"\n}"
